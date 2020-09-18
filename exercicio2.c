e1f32b1ace2bed1ba6b2174d6d12a2e7ba18ddaa
dethales <thaleslayber@gmail.com>
2020-09-17 22:26:49 -0300

    exercicio 2 17/09

--- exercicio2.c
+++ exercicio2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct aluno
 {
@@ -11,23 +12,51 @@ struct aluno
 };
 typedef struct aluno Aluno;
 
+Aluno* leAluno(void);
+
 int main(void)
 {
     int numAlunos;
     char* nomeAluno = NULL;
     size_t tamNomeAluno = 0;
     int i;
+    char c;
 
     printf("Digite o numero de alunos na turma: ");
     scanf("%d", &numAlunos);
+    while ((c = getchar()) != '\n' && c != EOF);    // consume o '\n' deixado pelo scanf
 
     Aluno** turma = (Aluno**) malloc(numAlunos * sizeof(Aluno*));
 
     for(i = 0; i < numAlunos; i++)
-        turma[i] = (Aluno*) malloc(sizeof(Aluno));
-
-    turma[0]->nome = "Joao";
-    printf("%s", turma[0]->nome);
+    {
+        turma[i] = leAluno();
+    }
 
     return 0;
+}
+
+Aluno* leAluno(void)
+{
+    Aluno* a = (Aluno*) malloc(sizeof(Aluno));
+    const size_t tamMaxNome = 128;
+    char c;
+    a->nome = (char*) malloc(tamMaxNome * sizeof(char));
+
+    printf("Digite o nome do Aluno:\n");
+    fgets(a->nome, tamMaxNome, stdin);
+    strtok(a->nome, "\n");  // retira o '\n' que o fgets captura no final da string
+
+    printf("Digite a matricula de %s: ", a->nome); // imprime so o primeiro nome
+    scanf("%d", &a->matricula);
+
+    printf("Digite a nota da P1 de %s: ", a->nome);
+    scanf("%f", &a->p1);
+    printf("Digite a nota da P2 de %s: ", a->nome);
+    scanf("%f", &a->p2);
+    printf("Digite a nota da P3 de %s: ", a->nome);
+    scanf("%f", &a->p3);
+    while ((c = getchar()) != '\n' && c != EOF);    // consume o '\n' deixado pelo scanf
+
+    return a;
 }
